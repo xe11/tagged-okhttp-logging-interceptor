2783c1cc81ac1ee3090d88b069726f72af7e9ecd
xe11 <alexei.laban@gmail.com>
2024-01-04 20:45:23 +0200

    #2: replace UUID/"123"/"id123" by "Id" in url tag

--- tagged-logger/src/main/java/xe11/ok/logger/tag/UrlTagProvider.kt
+++ tagged-logger/src/main/java/xe11/ok/logger/tag/UrlTagProvider.kt
@@ -2,6 +2,18 @@ package xe11.ok.logger.tag
 
 import okhttp3.Request
 import java.util.Locale
+import java.util.regex.Pattern
+
+private val UUID_REGEX: Pattern =
+    Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
+private val NUMERIC_REGEX: Pattern = Pattern.compile("^\\d{2,}$")
+private val ID_NUMERIC_REGEX: Pattern = Pattern.compile("^id\\d{2,}$")
+
+private val PATH_SEGMENTS_REPLACEMENTS: List<Pattern> = listOf(
+    UUID_REGEX,
+    NUMERIC_REGEX,
+    ID_NUMERIC_REGEX,
+)
 
 internal class UrlTagProvider : TagProvider {
 
@@ -18,6 +30,23 @@ internal class UrlTagProvider : TagProvider {
             return emptyList()
         }
 
+        val segments = prepareSegments(pathSegments)
+
+        return createShortenedTag(segments, maxSize)
+    }
+
+    private fun prepareSegments(pathSegments: List<String>): List<String> {
+        return pathSegments.map { segment ->
+            val isId = PATH_SEGMENTS_REPLACEMENTS.any { it.matcher(segment).matches() }
+            if (isId) "Id" else segment
+        }
+    }
+
+    private fun createShortenedTag(
+        pathSegments: List<String>,
+        maxSize: Short,
+    ): List<String> {
+        val segmentsSize = pathSegments.size
         var includedSegmentsCount = segmentsSize
         var chunkSize = maxSize / segmentsSize
         while (chunkSize < 2) {
