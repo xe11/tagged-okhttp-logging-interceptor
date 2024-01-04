9a4e93a9e75e696fe706b84912a863594b6fcbec
xe11 <alexei.laban@gmail.com>
2024-01-04 21:42:50 +0200

    #2: add tests for corner cases

--- tagged-logger/src/test/java/xe11/ok/logger/tag/UrlTagProviderTest.kt
+++ tagged-logger/src/test/java/xe11/ok/logger/tag/UrlTagProviderTest.kt
@@ -22,18 +22,28 @@ internal class UrlTagProviderTest {
         )
 
         val testData = listOf(
-            TestData("/", 10, ""),
+            TestData(path = "/", length = 10, expected = ""),
             TestData("", 10, ""),
             TestData("/Capitalized", 10, "Capitalize"),
             TestData("/test/end/point", 10, "TesEndPoi"),
             TestData("/long/test/end/point", 4, "EnPo"),
             TestData("/test/end/point/a/b/c/d", 4, "CD"),
             TestData("/test/end/point/a/b/c/d", 4, "CD"),
+            TestData("/quite/long/test/end/point", 6, "TeEnPo"),
             TestData("/test/$VALID_UUID", 10, "TestId"),
             TestData("/test/2692951-237c-11ed-861d-0242ac120002", 10, "Test26929"),
             TestData("/test/26-929514237c-11ed861d-0242ac12-0002", 10, "Test26-92"),
             TestData("/test/123", 10, "TestId"),
             TestData("/test/id123", 10, "TestId"),
+            // corner cases
+            TestData("/", 0, ""),
+            TestData("/", -1, ""),
+            TestData("/Capitalized", 0, ""),
+            TestData("/Capitalized", -1, ""),
+            TestData("/%F0%9F%A4%94/", 10, "\uD83E\uDD14"),
+            TestData("/\uD83E\uDD14/asdf", 10, "\uD83E\uDD14Asdf"),
+            TestData("/\uD83E\uDD14asdf", 10, "\uD83E\uDD14asdf"),
+            TestData("/\uD83E\uDD14asdfghj", 4, "\uD83E\uDD14as"),
         )
 
         return testData.toDynamicTest { (path, length, expected) ->
